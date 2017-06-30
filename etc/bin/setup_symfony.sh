eba7a59b1f4f5e32597abe88be821a46ac68bad7
nstacey <nick.stacey@freelance.timeinc.com>
2017-06-30 12:48:05 +0100

    Minor fix for .gitignore merging

--- etc/bin/setup_symfony.sh
+++ etc/bin/setup_symfony.sh
@@ -3,5 +3,5 @@
 composer create-project symfony/framework-standard-edition temp
 rm -rf web
 mv -f temp/* .
-paste temp/.gitignore .gitignore > .gitignore
+cat temp/.gitignore >> .gitignore
 rm -rf temp
